9d4d67868f7557ebbe6961aac838815fa1a0ee7a
Hubert Jr <hubertjunior254@icloud.com>
2022-11-25 12:50:58 +0300

    work complete

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// code your solution here
+function saturdayFun(activity){
+    if (activity === "bathe my dog!"){
+        return "This Saturday, I want to bathe my dog!"
+    }else{
+        return "This Saturday, I want to roller-skate!"
+    }
+}
+
+const mondayWork = function(location){
+    if (location === "work from home"){
+        return "This Monday, I will work from home."
+    }else {
+        return "This Monday, I will go to the office."
+    }
+
+}
+
+function wrapAdjective(init){
+   let text1 = "You are"
+   const text2 = "!"
+   return function A(){
+    const textvar = "a hard worker"
+    const textvar2 = "a dedicated programmer"
+    if (init === "*"){
+        console.log(`${text1} ${wrapAdjective(init)(textvar)} $(text2)`)
+    }
+   }
+
+
+
+
+}
